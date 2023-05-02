a23ebdb1c281c2d9fd71a3b82ac8825499d96a89
VigneshSK17 <vigsk17@gmail.com>
2023-05-01 21:21:21 -0400

    Added docker and go chi

---
+++ src/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+
+	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Recoverer)
+
+	r.Use(middleware.Timeout(60 * time.Second))
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Cubimer API"))
+	})
+
+	http.ListenAndServe(":8080", r)
+
+}
